00ea3228c0b056db5ff8234d95179118df9ae646
m1404639 <m1404639@globalia.com>
2022-08-21 02:31:02 +0530

    implementation of font manager

---
+++ lib/presentation/style_manager.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced/presentation/font_manager.dart';
+
+TextStyle getTextStyle(
+    {required double fontSize,
+    String fontFamily = FontConstants.fontFamily,
+    required FontWeight fontWeight,
+    required Color color}) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    fontWeight: fontWeight,
+    color: color,
+  );
+}
+
+TextStyle getRegularTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: FontWeightManager.regular,
+    color: color,
+  );
+}
+
+TextStyle getMediumTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: FontWeightManager.medium,
+    color: color,
+  );
+}
+
+TextStyle getLightTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: FontWeightManager.light,
+    color: color,
+  );
+}
+
+TextStyle getSemiBoldTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: FontWeightManager.semiBold,
+    color: color,
+  );
+}
+
+TextStyle getBoldTextStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: FontWeightManager.bold,
+    color: color,
+  );
+}
